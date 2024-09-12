ce4e13623e9a6caf623830ae3aa44163c99d0f8e
Elamin <elaminhamid.elameen@elev.ga.ntig.se>
2024-09-12 09:43:26 +0200

    why so serious

--- kung.py
+++ kung.py
@@ -8,28 +8,34 @@
 from random import randint 
 
 play_game = "J"
-    player_one_score = 0
-    player_two_score = 0
+player_one_score = 0
+player_two_score = 0
     
 
-while play_game. upper() = "J":
+while play_game.upper() == "J":
 
 
     player_one_roll = randint(1,6)
     player_two_roll = randint(1,6) 
    
     if player_one_roll > player_two_roll:
-        print(f"spelare ett vinner med tärningkastet")
+        print(f"spelare ett vinner")
 
     elif player_two_roll > player_one_roll: 
-        print(f"yes")
+        print(f"spelare två vinner")
     else: 
-        print(f"ingen spelare vinner, det är oavgjort ned tärningskastet")
+        print(f"ingen spelare vinner")
 
-    play_game = input("vill du spela igen?:[J/N]: ")
+elif player_two_score > 2: 
+
+play_game = input("vill du spela igen?:[J/N]:")
+
+
 # at göra, komma ihåg vilken spelare som har vunnit
 # spela tills vi har en bäst av tre
 # sedan fråga om vi vill spela igen    
+
+
 
 
 
